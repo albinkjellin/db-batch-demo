c5479d2cb201eed28ace38e2b8930cf4c6fba70f
albinkjellin <albin.kjellin@mulesoft.com>
2014-03-06 17:24:53 -0800

    Initial commit

---
+++ src/main/resources/db/tables.sql
@@ -0,0 +1,9 @@
+CREATE  TABLE `batch`.`database1` (
+  `id` INT NOT NULL ,
+  `status` VARCHAR(45) NULL ,
+  PRIMARY KEY (`id`) );
+
+CREATE  TABLE `batch`.`database2` (
+  `id` INT NOT NULL ,
+  `status` VARCHAR(45) NULL ,
+  PRIMARY KEY (`id`) );
